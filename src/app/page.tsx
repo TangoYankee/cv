83ded754146bd5e23fdb4bf6a953529edcd56803
tangoyankee <miller.tim108@gmail.com>
2023-07-10 21:01:01 -0400

    correct gh link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <div className={styles.grid}>
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          href="https://github.com/tangoyankee/"
           className={styles.card}
           target="_blank"
           rel="noopener noreferrer"
